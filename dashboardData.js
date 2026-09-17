282a3244958ca2bacfa4fec08cad70d52d953804
unknown <you@example.com>
2026-09-17 22:51:49 +0530

    Update dashboard data and society shares

--- dashboardData.js
+++ dashboardData.js
@@ -9,8 +9,8 @@ const dashboardData =
 
 {
     "totalFamilies":  155,
-    "totalMembers":  805,
-    "grandTotal":  8997571,
+    "totalMembers":  804,
+    "grandTotal":  8853821,
     "freeWill":  551515,
     "faithPromise":  0,
     "employeeSubscription":  4035413,
@@ -28,7 +28,7 @@ const dashboardData =
     "windows":  661500,
     "cpc":  0,
     "pillars":  810000,
-    "tiles":  287500,
+    "tiles":  143750,
     "savingBox":  2249343,
     "families":  [
                      {
@@ -1266,10 +1266,10 @@ const dashboardData =
                      {
                          "leikai":  "Group",
                          "head":  "Mother Teresa Group",
-                         "members":  2,
+                         "members":  1,
                          "savingBox":  0,
-                         "tiles":  143750,
-                         "total":  143950
+                         "tiles":  0,
+                         "total":  200
                      }
                  ],
     "memberDirectory":  [
@@ -19763,29 +19763,6 @@ const dashboardData =
                                 "cpc":  0,
                                 "pillars":  0,
                                 "tiles":  0,
-                                "savingBox":  0
-                            },
-                            {
-                                "head":  "Mother Teresa Group",
-                                "leikai":  "Group",
-                                "member":  "",
-                                "relationship":  "",
-                                "freeWill":  0,
-                                "faithPromise":  0,
-                                "phase1":  0,
-                                "phase2":  0,
-                                "phase3":  0,
-                                "phase4":  0,
-                                "phase5":  0,
-                                "phaseA":  0,
-                                "phaseB":  0,
-                                "phaseC":  0,
-                                "phaseD":  0,
-                                "phaseE":  0,
-                                "windows":  0,
-                                "cpc":  0,
-                                "pillars":  0,
-                                "tiles":  143750,
                                 "savingBox":  0
                             }
                         ]
